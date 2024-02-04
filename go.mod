e855640bf9530394f40bbd96fc0627ad0fcc2687
ono5 <kuroneko_head_lord@yahoo.co.jp>
2024-02-04 18:31:13 +0900

    add db and connect

--- go.mod
+++ go.mod
@@ -2,8 +2,9 @@ module github.com/ono5/jwt-with-golang
 
 go 1.21.1
 
+require github.com/gorilla/mux v1.8.1
+
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,54 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/subosito/gotenv"
 )
 
+type User struct {
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type JWT struct {
+	Token string `json:"token"`
+}
+
+type Error struct {
+	Message string `json:"message"`
+}
+
+var db *sql.DB
+
+func init() {
+	gotenv.Load()
+}
+
 func main() {
+	postgresURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("USER"),
+		os.Getenv("PASSWORD"),
+		os.Getenv("HOST"),
+		os.Getenv("PORT"),
+		os.Getenv("DBNAME"))
+
+	db, err := sql.Open("postgres", postgresURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	router := mux.NewRouter()
 
 	router.HandleFunc("/signup", signup).Methods("POST")
