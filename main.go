5f8e117d4001fc5efd1d835c9b84aeb8b7d84f72
ono5 <kuroneko_head_lord@yahoo.co.jp>
2024-02-04 16:31:05 +0900

    Setup Server and Test handler functions

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/signup", signup).Methods("POST")
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/protected", TokenVerifyMiddleware(protectedEndpoint)).Methods("GET")
+
+	log.Println("Liten on port 8000...")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("successfully called signup"))
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("successfully called login"))
+}
+
+func protectedEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("protectedEndpoint invoked.")
+}
+
+func TokenVerifyMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	fmt.Println("TokenVerifyMiddleware invoked.")
+	return nil
 }
