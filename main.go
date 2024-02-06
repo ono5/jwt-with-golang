44431acce9bba410bad09334d033009061ef7113
ono5 <kuroneko_head_lord@yahoo.co.jp>
2024-02-06 15:24:10 +0900

    refactor code

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/ono5/jwt-with-golang/controllers"
 	"github.com/ono5/jwt-with-golang/driver"
+	"github.com/ono5/jwt-with-golang/utils"
 	"github.com/subosito/gotenv"
 )
 
@@ -18,17 +19,14 @@ func init() {
 }
 
 func main() {
-
 	db = driver.ConnectDB()
-
 	router := mux.NewRouter()
 
 	controller := controllers.Controller{}
 
+	router.HandleFunc("/protectedEndpoint", utils.TokenVerifyMiddleWare(controller.ProtectedEndpoint())).Methods("GET")
 	router.HandleFunc("/signup", controller.Signup(db)).Methods("POST")
 	router.HandleFunc("/login", controller.Login(db)).Methods("POST")
-	router.HandleFunc("/protected", controller.TokenVerifyMiddleware(controller.ProtectedEndpoint())).Methods("GET")
 
-	log.Println("Liten on port 8000...")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
--- utils/utils.go
+++ utils/utils.go
@@ -2,9 +2,11 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/ono5/jwt-with-golang/models"
@@ -45,4 +47,42 @@ func GenerateToken(user models.User) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var errorObject models.Error
+		authHeader := r.Header.Get("Authorization")
+		bearerToken := strings.Split(authHeader, " ")
+
+		if len(bearerToken) == 2 {
+			authToken := bearerToken[1]
+
+			token, error := jwt.Parse(authToken, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an error")
+				}
+
+				return []byte("secret"), nil
+			})
+
+			if error != nil {
+				errorObject.Message = error.Error()
+				RespondWithError(w, http.StatusUnauthorized, errorObject)
+				return
+			}
+
+			if token.Valid {
+				next.ServeHTTP(w, r)
+			} else {
+				errorObject.Message = error.Error()
+				RespondWithError(w, http.StatusUnauthorized, errorObject)
+				return
+			}
+		} else {
+			errorObject.Message = "Invalid token."
+			RespondWithError(w, http.StatusUnauthorized, errorObject)
+			return
+		}
+	})
 }
